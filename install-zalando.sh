eb869e2d53462f6eeada13d2ef2aad1846442fb6
FunGuardian <seski.ramadhan@gmail.com>
2022-02-28 22:01:43 +0700

    update readme

--- install-zalando.sh
+++ install-zalando.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/zalando/postgres-operator.git
 helm install postgres-operator ./postgres-operator/charts/postgres-operator
 helm install postgres-operator-ui ./postgres-operator/charts/postgres-operator-ui
-kubectl port-forward svc/postgres-operator-ui 8081:80
+#kubectl port-forward svc/postgres-operator-ui 8081:80
 kubectl create -f postgres-operator/manifests/minimal-postgres-manifest.yaml
